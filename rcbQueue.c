64618c724d07d272b8f58a6167abe0c3a858cf8e
Colton Campbell <ccampbell@bluenose.cs.dal.ca>
2017-04-02 14:40:17 -0300

    Finished MLFB and SJF

--- rcbQueue.c
+++ rcbQueue.c
@@ -49,3 +49,40 @@ struct RCB Dequeue(struct Queue *queue) {
         }
 }
 
+void Insert(struct Queue *queue, struct RCB newRCB){
+	struct Node *newNode = malloc(sizeof(struct Node));
+        newNode->data = newRCB;
+
+   	if(queue->front == NULL){
+		queue->front = newNode;
+		newNode->next = NULL;
+	}
+	else{
+		if(queue->front->data.bytesRemaining > newRCB.bytesRemaining){
+			newNode->next = queue->front;
+			queue->front = newNode;
+		}
+		else{
+			struct Node *current = queue->front;
+
+			while(1){
+				if(current->next == NULL){
+					current->next = newNode;
+					newNode->next = NULL;
+					break;
+				}
+				else{
+					if(current->next->data.bytesRemaining > newRCB.bytesRemaining){
+						newNode->next = current->next;
+						current->next = newNode;
+						break;
+					}
+					else{
+						current = current->next;
+					}
+				}
+			}
+		}
+	}
+
+}
